64544e2ab5d01d321d618edab50cdf184769d23b
wangjinyu124419 <wangjinyu124419@163.com>
2019-05-19 20:40:23 +0800

    字典

--- 4_字典/4.2_字典.py
+++ 4_字典/4.2_字典.py
@@ -6,3 +6,20 @@ d2=dict.fromkeys(['a','b','c'])
 d3=dict.fromkeys(['a','b','c'],True)
 print(d2)
 print(d3)
+
+print(d1.pop('wang'))
+print(d1)
+
+print(d1.popitem())
+print(d1)
+# setdefault
+d4={'a':1,'b':2}
+d4.setdefault('a','2')
+d4.setdefault('c','2')
+print(d4)
+
+#update更新字典
+d1={'a':1,'c':3}
+d2={'b':2,'c':4}
+print(d1.update(d2))
+print(d1)
